f8bcdacd4df4b2eec173fc50e2fb272d3bd075b7
JAVED12-hash <javedroxx4@gmail.com>
2024-09-28 03:58:20 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,33 +3,33 @@
 //YouTube: @GlobalTechInfo
 //Telegram: https://t.me/GlobalTechInc
 //GitHub: @GlobalTechInfo
-//WhatsApp: https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@GlobalTechInfo
+//WhatsApp: https://whatsapp.com/channel/0029VaajfWh4CrfZFMatGh45
+//want more free bot scripts? subscribe to my youtube channel: https://whatsapp.com/channel/0029VaajfWh4CrfZFMatGh45
 
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
+global.ownernomer = "447934915388"
 global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ytname = "Homelander"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '447934915388'  //creator number
+global.ownername = 'homelander' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: '+447934915388'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaajfWh4CrfZFMatGh45'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
